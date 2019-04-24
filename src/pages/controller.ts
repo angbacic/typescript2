3ce88fc483901a9b4f1a14f7dc79fee6c54d69f8
angbacic <angela.bacic1@gmail.com>
2019-04-24 14:10:53 +0200

    get,post,put

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,19 +1,43 @@
-import { JsonController, Get, Param } from 'routing-controllers'
-import pagesById, { Page } from './data'
+import { JsonController, Get, Param, Put,Body, NotFoundError,HttpCode,Post } from 'routing-controllers'
+// , , Post, HttpCode}
+// import pagesById, { Page } from './data'
+import Page from './entity'
 
-type PageList = { pages: Page[] }
+// type PageList = { pages: Page[] }
 
 @JsonController()
 export default class PageController {
 
     @Get('/pages/:id')
-    getPage(
-        @Param('id') id: number
-    ): Page {
-        return pagesById[id]
-    }
-    @Get ('/pages')
-    allPages (): PageList{
-        return { pages : Object.values(pagesById)}
-    }
+getPage(
+  @Param('id') id: number
+) {
+  return Page.findOne(id)
+}
+
+
+@Get('/pages')
+async allPages() {
+  const pages =await Page.find()
+  return { pages }
+}
+
+@Put('/pages/:id')
+async updatePage(
+  @Param('id') id: number,
+  @Body() update: Partial<Page>
+) {
+  const page = await Page.findOne(id)
+  if (!page) throw new NotFoundError('Cannot find page')
+
+  return Page.merge(page, update).save()
+}
+
+@Post('/pages')
+@HttpCode(201)
+createPage(
+  @Body() page: Page
+) {
+  return page.save()
+}
 }   
